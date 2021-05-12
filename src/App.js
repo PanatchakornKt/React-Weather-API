eed17a8dae9b594fb3845d5f64ffb6694b8fde82
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-05-12 20:10:37 +0700

    first version

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from './components/Head'
+import Form from './components/Form'
+
+function App() {
+
+  return (
+    <>
+      <div className = 'text-center'>
+        <div>
+          <Head></Head>
+        </div>
+        <Form />
+      </div>
+    </>
+  )
+}
+
+export default App;
+
+
---
+++ src/components/Head.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <div >
+        <div className="hero-headline flex flex-col items-center justify-center pt-10 text-center">
+            <h1 className=" font-bold text-3xl text-blue-900">
+            Weather App</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Head;
