8798ccf6f754ead1ab4e0352f86f5697fda6db2a
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-05-13 17:35:19 +0700

    refactor code

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Head from "./Head";
+import SearchWeather from "./SearchWeather";
+import DisplayWeather from "./DisplayWeather";
+
+const App = () => {
+  const [weather, setWeather] = useState([]);
+
+  return (
+    <React.Fragment>
+      <Head />
+      <SearchWeather weather={weather} setWeather={setWeather} />
+      <DisplayWeather weather={weather} />
+    </React.Fragment>
+  );
+};
+
+export default App;
